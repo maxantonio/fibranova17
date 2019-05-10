e3ef20bd2feb18717c4b86130216307c94bb78b3
Tania <tbarrosolopez@gmail.com>
2019-05-10 17:28:32 -0400

    subiendo app base

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from os import name
 
+from django.urls import path
 from . import views
 
+app_name = 'frontend'
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('contacto/', views.contacto, name='contacto'),
+    path('perfil/nosotros', views.perfil_nosotros, name='perfil-nosotros'),
+    path('perfil/presencia-geografica', views.presencia_geografica, name='perfil-presencia-geografica'),
+    path('perfil/historia', views.historia, name='perfil-historia'),
+    path('perfil/estrategia', views.estrategia, name='perfil-estrategia'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 # Create your views here.
 def index(request):
-
-
-    context = {
-        'meta_description': "Base del meta"}
+    context = {'title': 'Index'}
     return render(request, 'frontend/index.html', context)
+
+def contacto(request):
+    context = {'title': 'Contacto'}
+    return render(request, 'frontend/contacto.html', context)
+
+# PERFIL
+def perfil_nosotros(request):
+    context = {'title': 'Nosotros'}
+    return render(request, 'frontend/perfil/nosotros.html', context)
+
+def presencia_geografica(request):
+    context = {'title': 'Presencia Geografica'}
+    return render(request, 'frontend/perfil/presencia_geografica.html', context)
+
+def historia(request):
+    context = {'title': 'Historia'}
+    return render(request, 'frontend/perfil/historia.html', context)
+
+
+def estrategia(request):
+    context = {'title': 'Estrategia'}
+    return render(request, 'frontend/perfil/estrategia.html', context)
+
+
+# END PERFIL
