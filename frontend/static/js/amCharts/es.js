23918e1ea005a5084b76b4d4bc40898270c02ea0
Yoidel <yoidel86@gmail.com>
2021-06-03 17:59:47 -0400

    ajustes calculadora y cotizacion

---
+++ frontend/static/js/amCharts/es.js
@@ -0,0 +1,7 @@
+AmCharts.monthNames = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    AmCharts.shortMonthNames = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+AmCharts.dayNames = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+AmCharts.shortDayNames = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
+AmCharts.fromText = "Desde: ";
+//     "zoomOutText": "Mostrar todos"
+// }
