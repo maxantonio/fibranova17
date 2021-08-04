f392697886feff354a5a76d5f284462bf20ec3bb
Tania <tbarrosolopez@gmail.com>
2021-08-04 17:36:29 -0400

    cambios generales

--- frontend/static/js/amCharts/contenido/indicadores.js
+++ frontend/static/js/amCharts/contenido/indicadores.js
@@ -1,9 +1,9 @@
-var chart = AmCharts.makeChart( "pie_1", {
+var chart = AmCharts.makeChart("pie_1", {
     "type": "pie",
     "theme": "none",
-    // "colors":["#1F478D","#666","#ddd"],
+    "colors": ["#1F478D", "#E11D50", "#08193B", "#B81F3C", "#4F709E", "#F16991", "#8EBEE6"],
     "legend": {
-        valueText : '' //or \n if you want new lines
+        valueText: '' //or \n if you want new lines
     },
     "dataProvider": [{
         "country": "Industrial",
@@ -24,26 +24,27 @@ var chart = AmCharts.makeChart( "pie_1", {
     "valueField": "litres",
     "titleField": "country",
     // "labelsEnabled":false,
-    "labelText":"[[value]]%",
-    "labelRadius":-40,
-    "baseColor":"#1F478D",
-    "color":"#fff",
-    "balloon":{
-        "fixedPosition":true
-    }   ,
-    balloonText:"[[title]]:[[value]]%"
+    "labelText": "[[value]]%",
+    "labelRadius": -40,
+    "radius": "40%",
+    // "baseColor":"#1F478D",
+    "color": "#fff",
+    "balloon": {
+        "fixedPosition": true
+    },
+    balloonText: "[[title]]:[[value]]%"
 
 
-} );
+});
 
-var chart = AmCharts.makeChart( "pie_2", {
+var chart = AmCharts.makeChart("pie_2", {
     "type": "pie",
     "theme": "none",
-    // "colors":["#1F478D","#666","#ddd"],
+    "colors": ["#1F478D", "#E11D50", "#08193B", "#B81F3C", "#4F709E", "#F16991", "#8EBEE6"],
     "legend": {
-        valueText : '' //or \n if you want new lines
+        valueText: '' //or \n if you want new lines
     },
-    "dataProvider": [ {
+    "dataProvider": [{
         "country": "Chih",
 
         "litres": 56
@@ -69,27 +70,28 @@ var chart = AmCharts.makeChart( "pie_2", {
     "valueField": "litres",
     "titleField": "country",
     // "labelsEnabled":false,
-    "labelText":"[[value]]%",
-    "labelRadius":-20,
-    "baseColor":"#1F478D",
-    "color":"#fff",
-    "balloon":{
-        "fixedPosition":true
+    "labelText": "[[value]]%",
+    "labelRadius": -20,
+    "radius": "40%",
+    // "baseColor":"#1F478D",
+    "color": "#fff",
+    "balloon": {
+        "fixedPosition": true
     },
-    balloonText:"[[title]]:[[value]]%"
+    balloonText: "[[title]]:[[value]]%"
 
-} );
+});
 
-var chart = AmCharts.makeChart( "pie_3", {
+var chart = AmCharts.makeChart("pie_3", {
     "type": "pie",
     "theme": "none",
-    // "colors":["#1F478D","#666","#ddd"],
+    "colors": ["#1F478D", "#E11D50", "#08193B", "#B81F3C", "#4F709E", "#F16991", "#8EBEE6"],
     "legend": {
-        valueText :'\n', //or \n if you want new lines
-        maxColumns:1,
-        "align":"center"
+        valueText: '\n', //or \n if you want new lines
+        maxColumns: 1,
+        "align": "center"
     },
-    "dataProvider": [ {
+    "dataProvider": [{
         "country": "Ocupado",
 
         "litres": 100
@@ -100,27 +102,28 @@ var chart = AmCharts.makeChart( "pie_3", {
     "valueField": "litres",
     "titleField": "country",
     // "labelsEnabled":false,
-    "labelText":"[[value]]%",
-    "labelRadius":-90,
-    "baseColor":"#1F478D",
-    "color":"#fff",
-    "balloon":{
-        "fixedPosition":true
-    },   balloonText:"[[title]]:[[value]]%"
+    "labelText": "[[value]]%",
+    "labelRadius": -90,
+    "radius": "40%",
+    // "baseColor":"#1F478D",
+    "color": "#fff",
+    "balloon": {
+        "fixedPosition": true
+    }, balloonText: "[[title]]:[[value]]%"
 
 
-} );
+});
 
-var chart = AmCharts.makeChart( "pie_4", {
+var chart = AmCharts.makeChart("pie_4", {
     "type": "pie",
     "theme": "none",
-    // "colors":["#1F478D","#666","#ddd"],
+    "colors": ["#1F478D", "#E11D50", "#08193B", "#B81F3C", "#4F709E", "#F16991", "#8EBEE6"],
     "legend": {
-        valueText : '\n', //or \n if you want new lines
-        maxColumns:1,
-        "align":"center"
+        valueText: '\n', //or \n if you want new lines
+        maxColumns: 1,
+        "align": "center"
     },
-    "dataProvider": [ {
+    "dataProvider": [{
         "country": "MXN",
 
         "litres": 57
@@ -131,13 +134,148 @@ var chart = AmCharts.makeChart( "pie_4", {
     "valueField": "litres",
     "titleField": "country",
     // "labelsEnabled":false,
-    "labelText":"[[value]]%",
-    "labelRadius":-40,
-    "baseColor":"#1F478D",
-    "color":"#fff",
-    "balloon":{
-        "fixedPosition":true
-    },    balloonText:"[[title]]:[[value]]%"
+    "labelText": "[[value]]%",
+    "labelRadius": -40,
+    "radius": "40%",
+    // "baseColor":"#1F478D",
+    "color": "#fff",
+    "balloon": {
+        "fixedPosition": true
+    }, balloonText: "[[title]]:[[value]]%"
 
+});
 
-} );
+if (locale == "es") {
+    var sf_activo = "Activo Total";
+    var sf_pasivo = "Pasivo total";
+    var sf_capital = "Capital Contable";
+    var sf_efectivo = "Efectivo y Equivalentes de Efectivo";
+    var sf_propiedades = "Propiedades de Inversión";
+} else {
+    var sf_activo = "Activo Total";
+    var sf_pasivo = "Pasivo total";
+    var sf_capital = "Capital Contable";
+    var sf_efectivo = "Efectivo y Equivalentes de Efectivo";
+    var sf_propiedades = "Propiedades de Inversión";
+}
+
+var chart5 = AmCharts.makeChart("chart_5", {
+    "type": "serial",
+    "theme": "light",
+    "categoryField": "year",
+    "rotate": false,
+    "startDuration": 1,
+    "colors": ["#1F478D", "#E11D50", "#08193B", "#B81F3C", "#4F709E", "#F16991", "#8EBEE6"],
+    "categoryAxis": {
+        "gridPosition": "start",
+        "position": "left",
+        "gridAlpha": 0,
+    },
+    // "titles": [
+    //     {
+    //         "text": "Situación Financiera",
+    //         "size": 15
+    //     },
+    //     {
+    //         "text": "(Miles de pesos mexicanos)",
+    //         "size": 12
+    //     }
+    // ],
+    "trendLines": [],
+    "graphs": [
+        {
+            "balloonText": "[[value]]",
+            // "balloonText": sf_activo + ": $[[value]]",
+            "fillAlphas": 1,
+            "id": "AmGraph-1",
+            "lineAlpha": 0.2,
+            // "title": sf_activo,
+            "type": "column",
+            "valueField": "sf_activo",
+            "labelText": "[[sf_activo_d]]"
+        },
+    ],
+    "guides": [],
+    "valueAxes": [
+        {
+            // "unit": "$",
+            "unitPosition": "left",
+            "id": "ValueAxis-1",
+            "position": "left",
+            "axisAlpha": 0,
+            "gridAlpha": 0,
+            "labelsEnabled": false
+        }
+    ],
+    // "legend": {
+    //     "align": "center",
+    //     "position": "bottom",
+    //     "right": -4,
+    //
+    //     "color": "gray",
+    //     "textClickEnabled": true
+    // },
+    "allLabels": [],
+    "balloon": {},
+    "dataProvider": [
+        {
+            "year": "2021",
+            "sf_activo": 8824,
+            "sf_activo_d": "8,824",
+        },
+        {
+            "year": "2022",
+            "sf_activo": 38434,
+            "sf_activo_d": "38,434",
+        },
+        {
+            "year": "2023",
+            "sf_activo": 27193,
+            "sf_activo_d": "27,193",
+        },
+        {
+            "year": "2024",
+            "sf_activo": 5994,
+            "sf_activo_d": "5,994",
+        },
+        {
+            "year": "2025",
+            "sf_activo": 45914,
+            "sf_activo_d": "45,914",
+        },
+        {
+            "year": "2026",
+            "sf_activo": 189617,
+            "sf_activo_d": "189,617",
+        },
+        {
+            "year": "2027",
+            "sf_activo": 61189,
+            "sf_activo_d": "61,189",
+        },
+        {
+            "year": "2028",
+            "sf_activo": 152951,
+            "sf_activo_d": "152,951",
+        },
+        {
+            "year": "2029",
+            "sf_activo": 25925,
+            "sf_activo_d": "25,925",
+        },
+        {
+            "year": "2030",
+            "sf_activo": 53797,
+            "sf_activo_d": "53,797",
+        },
+        {
+            "year": "2031",
+            "sf_activo": 38485,
+            "sf_activo_d": "38,485",
+        }
+    ],
+    "export": {
+        "enabled": true
+    }
+
+});
