25ff253970c2359176801bf9d2cdb2b0ac69be07
Tania <tbarrosolopez@gmail.com>
2021-08-26 13:59:55 -0400

    trabajando en las vistas en ingles

---
+++ frontend/static/js/amCharts/contenido/dashboard-en.js
@@ -0,0 +1,351 @@
+var loaded = 0;
+var locale = "es";
+var ipc = [];
+var precios = [];
+var frmex = [];
+var intradia = {};
+ticker_symbol = "FNOVA17";
+
+function intradiaF(data) {
+    Datatabla(data.intradia)
+}
+
+function historicos(json) {
+    precios = json.precios;
+    loaded += 1;
+    if (loaded == 3) {
+        fillData(precios, ipc, frmex, ticker_symbol);
+    }
+}
+
+function frmexLoaded(json) {
+    frmex = json.precios;
+    loaded += 1;
+    if (loaded == 3) {
+        // Datatabla(intradia);
+        fillData(precios, ipc, frmex, ticker_symbol);
+    }
+    //            console.dir(json.sites);
+}
+
+function ipcLoaded(json) {
+    ipc = json.precios;
+    loaded += 1;
+    if (loaded == 3) {
+        // Datatabla(intradia);
+        fillData(precios, ipc, frmex, ticker_symbol);
+    }
+    //            console.dir(json.sites);
+}
+
+function cargarInfo() {
+    $.ajax({
+        url: 'https://hkpy.irstrat.com/intradia/170?callback=intradiaF',
+        async: true,
+        dataType: 'jsonp',
+        jsonpCallback: 'intradiaF',
+        jsonp: 'intradiaF',
+        contentType: "application/json",
+
+    });
+    $.ajax({
+        url: 'https://hkpy.irstrat.com/intradia/history/170?start=2018-08-23&callback=historicos',
+        async: true,
+        dataType: 'jsonp',
+        jsonpCallback: 'historicos',
+        jsonp: 'historicos',
+        contentType: "application/json",
+    });
+    $.ajax({
+        //CARGANDO LOS DATOS DE FRMEX
+        url: 'https://hkpy.irstrat.com/intradia/history/12?start=2018-08-23&callback=frmexLoaded',
+        async: true,
+        dataType: 'jsonp',
+        jsonpCallback: 'frmexLoaded',
+        jsonp: 'frmexLoaded',
+        // jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+
+    });
+    $.ajax({
+        //CARGANDO LOS DATOS DE FRMEX
+        url: 'https://hkpy.irstrat.com/intradia/history/11?start=2018-08-23&callback=ipcLoaded',
+        async: true,
+        dataType: 'jsonp',
+        jsonpCallback: 'ipcLoaded',
+        jsonp: 'ipcLoaded',
+        contentType: "application/json",
+
+
+    });
+}
+
+$(function () {
+    $(".preloader").fadeOut();
+    setTimeout(cargarInfo, 100);
+    var elemento = $("#destacados").find(".selector a")[0];
+    $(elemento).click();
+});
+
+function Datatabla(intradia) {
+    $('#table-date').html(intradia["date"]);
+    $('#table-price').html(intradia["price"]);
+    $('#table-change').html(intradia["change"]);
+    $('#table-range').html(intradia["min"] + " - " + intradia["max"]);
+    $('#table-volume').html(intradia["volume"]);
+    $('#price-ticker').html("$" + intradia["price"]);
+    $('#change-ticker').html(intradia["change"]);
+}
+
+//filling empty values from non operation
+function fillData(precios, ipc, frmex, ticker_symbol) {
+    var lvva = 0;
+    var lvvo = 0;
+    precios_fill = $.map(precios, function (n, i) {
+        if (n.value !== null) {
+            lvva = n.value;
+            lvvo = n.volume
+        }
+        if (n.volume === null) {
+            lvvo = 0
+        }
+        return [{"date": n.date, "value": lvva, "volume": lvvo}];
+    }).reverse();
+
+    ipc_fill = $.map(ipc, function (n, i) {
+        if (n.value !== null) {
+            lvva = n.value;
+            lvvo = n.volume
+        }
+        if (n.volume === null) {
+            lvvo = 0
+        }
+        return [{"date": n.date, "value": lvva, "volume": lvvo}];
+    }).reverse();
+    frmex_fill = $.map(frmex, function (n, i) {
+        if (n.close !== null) {
+            lvva = n.close;
+            lvvo = n.volume
+        }
+        if (n.volume === null) {
+            lvvo = 0
+        }
+        return [{"date": n.date, "value": lvva, "volume": lvvo}];
+    }).reverse();
+    ajustado_fill = $.map(precios, function (n, i) {
+        // console.log(n);
+        if (n.close !== null) {
+            lvva = n.close;
+            lvvo = n.volume
+        }
+        if (n.volume === null) {
+            lvvo = 0
+        }
+        return [{"date": n.date, "value": lvva, "volume": lvvo}];
+    }).reverse();
+    AmCharts.theme = AmCharts.themes.light;
+
+///tercera
+//     AmCharts.ready(function () {
+    createStockChart();
+    // });
+}
+
+
+function createStockChart() {
+    var chart = new AmCharts.AmStockChart();
+    chart.dataDateFormat = "YYYY-MM-DD";
+    // chart.marginRight = 150;
+    // chart.marginLeft = 150;
+    // chart.autoMarginOffset = 150;
+    chart.pathToImages = location.protocol + '//' + location.host + "/static/js/amCharts/images/";
+
+
+    // DATASETS //////////////////////////////////////////
+    // create data sets first
+    var dataSet1 = new AmCharts.DataSet();
+    dataSet1.title = ticker_symbol;
+    dataSet1.fieldMappings = [{
+        fromField: "value",
+        toField: "value"
+    }, {
+        fromField: "volume",
+        toField: "volume"
+    }];
+    dataSet1.dataProvider = precios_fill;
+    dataSet1.categoryField = "date";
+
+    var dataSet2 = new AmCharts.DataSet();
+    dataSet2.title = "IPC";
+    dataSet2.fieldMappings = [{
+        fromField: "value",
+        toField: "value"
+    }, {
+        fromField: "volume",
+        toField: "volume"
+    }];
+    dataSet2.dataProvider = ipc_fill;
+    dataSet2.categoryField = "date";
+
+    var dataSet3 = new AmCharts.DataSet();
+    dataSet3.title = "FIBRAS RT";
+    dataSet3.fieldMappings = [{
+        fromField: "value",
+        toField: "value"
+    }, {
+        fromField: "volume",
+        toField: "volume"
+    }];
+    dataSet3.dataProvider = frmex_fill;
+    dataSet3.categoryField = "date";
+
+
+    // set data sets to the chart
+    chart.dataSets = [dataSet1, dataSet3, dataSet2];
+
+    var panelsSettings = new AmCharts.PanelsSettings();
+    panelsSettings.marginLeft = 10;
+    panelsSettings.marginRight = 10;
+
+    chart.panelsSettings = panelsSettings;
+    chart.colors = ["#1F478D", "#1F478D"];
+    // PANELS ///////////////////////////////////////////
+    // first stock panel
+    var stockPanel1 = new AmCharts.StockPanel();
+    stockPanel1.showCategoryAxis = false;
+    stockPanel1.title = "Historical prices at last close";
+    stockPanel1.percentHeight = 70;
+
+    // graph of first stock panel
+    var graph1 = new AmCharts.StockGraph();
+    graph1.marginRight = 150;
+    graph1.valueField = "value";
+    graph1.comparable = true;
+    graph1.compareField = "value";
+    graph1.bullet = "round";
+    graph1.hideBulletsCount = 32;
+    graph1.bulletBorderColor = "#1F478D";
+    graph1.bulletBorderAlpha = 1;
+    graph1.balloonText = "[[title]]: <b>[[value]]</b>";
+    graph1.compareGraphBalloonText = "[[title]]:<b>[[value]]</b>";
+    graph1.compareGraphBullet = "round";
+    graph1.compareGraphBulletBorderColor = "#1F478D";
+    graph1.compareGraphBulletBorderAlpha = 1;
+    stockPanel1.addStockGraph(graph1);
+
+    // create stock legend
+    var stockLegend1 = new AmCharts.StockLegend();
+    stockLegend1.periodValueTextComparing = "[[percents.value.close]]%";
+    stockLegend1.periodValueTextRegular = "[[value.close]]";
+    stockPanel1.stockLegend = stockLegend1;
+
+
+    // second stock panel
+    var stockPanel2 = new AmCharts.StockPanel();
+    stockPanel2.title = "Volume";
+    stockPanel2.percentHeight = 30;
+    var graph2 = new AmCharts.StockGraph();
+    graph2.valueField = "volume";
+    graph2.type = "column";
+    graph2.showBalloon = true;
+    graph2.fillAlphas = 1;
+    graph2.periodValue = "Sum";
+    stockPanel2.addStockGraph(graph2);
+
+    var stockLegend2 = new AmCharts.StockLegend();
+    stockLegend2.periodValueTextRegular = "[[value.close]]";
+    stockPanel2.stockLegend = stockLegend2;
+
+    // set panels to the chart
+    chart.panels = [stockPanel1, stockPanel2];
+
+    // OTHER SETTINGS ////////////////////////////////////
+    var sbsettings = new AmCharts.ChartScrollbarSettings();
+    sbsettings.graph = graph1;
+    chart.chartScrollbarSettings = sbsettings;
+
+    // CURSOR
+    var cursorSettings = new AmCharts.ChartCursorSettings();
+    cursorSettings.valueBalloonsEnabled = true;
+    chart.chartCursorSettings = cursorSettings;
+
+
+    // PERIOD SELECTOR ///////////////////////////////////
+    var periodSelector = new AmCharts.PeriodSelector();
+    periodSelector.position = "left";
+    periodSelector.fromText = "From:";
+    periodSelector.toText = "To:";
+    periodSelector.periodsText = "Range:";
+
+    periodSelector.periods = [{
+        period: "MM",
+        count: 1,
+        label: "1 month"
+    }, {
+        period: "YYYY",
+        count: 1,
+        label: "1 Year"
+    }, {
+        period: "YYYY",
+        count: 5,
+        selected: true,
+        label: "5 years"
+    }];
+    chart.periodSelector = periodSelector;
+
+
+    // DATA SET SELECTOR
+    var dataSetSelector = new AmCharts.DataSetSelector();
+    dataSetSelector.position = "left";
+    dataSetSelector.selectText = "Select:";
+    dataSetSelector.compareText = "Compare:";
+
+    chart.dataSetSelector = dataSetSelector;
+
+    var categoryAxesSettings = new AmCharts.CategoryAxesSettings();
+    categoryAxesSettings.maxSeries = 0;
+    chart.categoryAxesSettings = categoryAxesSettings;
+
+    if (locale == 'es') {
+        chart.addListener('rendered', function (event) {
+            $(".amChartsPeriodSelector .amChartsInputField").datepicker({
+                dateFormat: (locale === 'es') ? "dd-mm-yy" : "mm/dd/yy",
+                closeText: "Cerrar",
+                prevText: "&#x3C;Ant",
+                nextText: "Sig&#x3E;",
+                currentText: "Hoy",
+                monthNames: ["enero", "febrero", "marzo", "abril", "mayo", "junio",
+                    "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+                monthNamesShort: ["ene", "feb", "mar", "abr", "may", "jun",
+                    "jul", "ago", "sep", "oct", "nov", "dic"],
+                dayNames: ["domingo", "lunes", "martes", "miércoles", "jueves", "viernes", "sábado"],
+                dayNamesShort: ["dom", "lun", "mar", "mié", "jue", "vie", "sáb"],
+                dayNamesMin: ["D", "L", "M", "X", "J", "V", "S"],
+                weekHeader: "Sm",
+                firstDay: 1,
+                isRTL: false,
+                showMonthAfterYear: false,
+                yearSuffix: ""
+            });
+        });
+    } else {
+        chart.addListener('rendered', function (event) {
+            $(".amChartsPeriodSelector .amChartsInputField").datepicker({
+                dateFormat: (locale === 'es') ? "dd-mm-yy" : "mm/dd/yy",
+            });
+        });
+    }
+
+    if ($(window).width() < 768) {
+        periodSelector.position = "top";
+        dataSetSelector.position = "top";
+        chart.periodSelector = periodSelector;
+        chart.dataSetSelector = dataSetSelector;
+        chart.write('chartdiv3');
+        $(".preloader").fadeOut();
+    } else {
+        chart.write('chartdiv3');
+        $(".preloader").fadeOut();
+
+    }
+}
--- frontend/views.py
+++ frontend/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from django.template import loader
+from django.utils.translation import gettext_lazy as _
 
 urlbase = 'http://cdn.investorcloud.net/FNOVA'
 
@@ -14,32 +15,32 @@ def index(request):
 
 def contacto(request):
     header_img = staticfiles_storage.url('images/background/contacto.png')
-    context = {'title': 'Contacto', 'header_img': header_img}
+    context = {'title': _('Contacto'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/contacto.html', context)
 
 
 def noticias(request):
     header_img = staticfiles_storage.url('images/background/noticias.png')
-    context = {'title': 'Noticias', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Noticias'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/noticias.html', context)
 
 
 def noticia(request, no):
     header_img = staticfiles_storage.url('images/background/noticias.png')
-    context = {'title': 'Noticia', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Noticia'), 'urlbase': urlbase, 'header_img': header_img}
     url = f'frontend/{request.LANGUAGE_CODE}/noticias/{no}.html'
     return render(request, url, context)
 
 
 def indicadores_operativos(request):
     header_img = staticfiles_storage.url('images/background/indicadores_operativos.png')
-    context = {'title': 'Indicadores Operativos', 'header_img': header_img}
+    context = {'title': _('Indicadores Operativos'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/indicadores.html', context)
 
 
 def portafolio(request, tab):
     header_img = staticfiles_storage.url('images/background/propiedades.png')
-    context = {'title': 'Propiedades',
+    context = {'title': _('Propiedades'),
                'tab': tab, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/portafolio.html', context)
 
@@ -47,31 +48,31 @@ def portafolio(request, tab):
 # PERFIL
 def perfil_nosotros(request):
     header_img = staticfiles_storage.url('images/background/nosotros.png')
-    context = {'title': 'Nosotros', 'header_img': header_img}
+    context = {'title': _('Nosotros'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/perfil/nosotros.html', context)
 
 
 def presencia_geografica(request):
     header_img = staticfiles_storage.url('images/background/presencia_geografica.png')
-    context = {'title': 'Presencia Geográfica', 'header_img': header_img}
+    context = {'title': _('Presencia Geográfica'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/perfil/presencia_geografica.html', context)
 
 
 def historia(request):
     header_img = staticfiles_storage.url('images/background/historia.png')
-    context = {'title': 'Historia', 'header_img': header_img}
+    context = {'title': _('Historia'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/perfil/historia.html', context)
 
 
 def estrategia(request):
     header_img = staticfiles_storage.url('images/background/estrategia.png')
-    context = {'title': 'Estrategia', 'header_img': header_img}
+    context = {'title': _('Estrategia'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/perfil/estrategia.html', context)
 
 
 def video(request):
     header_img = staticfiles_storage.url('images/background/estrategia.png')
-    context = {'title': 'Video Corporativo', 'header_img': header_img}
+    context = {'title': _('Video Corporativo'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/perfil/video.html', context)
 
 
@@ -230,39 +231,39 @@ def reportes_trimestrales(request):
 
 def reportes_anuales(request):
     header_img = staticfiles_storage.url('images/background/parallax-4.jpg')
-    context = {'title': 'Reportes Anuales', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Reportes Anuales'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/inversionistas/reportes-anuales.html', context)
 
 
 def fundamentales(request):
     header_img = staticfiles_storage.url('images/background/fundamentos.png')
-    context = {'title': 'Fundamentales', 'header_img': header_img}
+    context = {'title': _('Fundamentales'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/inversionistas/fundamentales.html', context)
 
 
 def como_invertir(request):
     header_img = staticfiles_storage.url('images/background/como_invertir.png')
-    context = {'title': '¿Cómo invertir en Fibra Nova?',
+    context = {'title': _('¿Cómo invertir en Fibra Nova?'),
                'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/inversionistas/como-invertir.html', context)
 
 
 def eventos_relevantes(request):
     header_img = staticfiles_storage.url('images/background/eventos_relevantes.png')
-    context = {'title': 'Eventos Relevantes',
+    context = {'title': _('Eventos Relevantes'),
                'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/inversionistas/eventos-relevantes.html', context)
 
 
 def faqs(request):
     header_img = staticfiles_storage.url('images/background/preguntas_frecuentes.png')
-    context = {'title': 'Preguntas Frecuentes', 'header_img': header_img}
+    context = {'title': _('Preguntas Frecuentes'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/inversionistas/faqs.html', context)
 
 
 def distribuciones(request):
     header_img = staticfiles_storage.url('images/background/distribuciones.png')
-    context = {'title': 'Distribuciones', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Distribuciones'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/bursatil/distribuciones.html', context)
 
 
@@ -271,25 +272,25 @@ def distribuciones(request):
 # BURSATIL
 def cotizacion(request):
     header_img = staticfiles_storage.url('images/background/comportamiento-accion.png')
-    context = {'title': 'Cotización', 'header_img': header_img}
+    context = {'title': _('Cotización'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/bursatil/cotizacion.html', context)
 
 
 def precio_historico(request):
     header_img = staticfiles_storage.url('images/background/precios-historicos.png')
-    context = {'title': 'Precio Histórico', 'header_img': header_img}
+    context = {'title': _('Precio Histórico'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/bursatil/precio-historico.html', context)
 
 
 def calculadora(request):
     header_img = staticfiles_storage.url('images/background/calculadora-rendimiento.png')
-    context = {'title': 'Calculadora de Rendimientos', 'header_img': header_img}
+    context = {'title': _('Calculadora de Rendimientos'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/bursatil/calculadora.html', context)
 
 
 def cobertura_analistas(request):
     header_img = staticfiles_storage.url('images/background/coberturas-analstas.png')
-    context = {'title': 'Cobertura de Analistas', 'header_img': header_img}
+    context = {'title': _('Cobertura de Analistas'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/bursatil/cobertura-analistas.html', context)
 
 
@@ -298,43 +299,43 @@ def cobertura_analistas(request):
 # GOBIERNO
 def directivos(request):
     header_img = staticfiles_storage.url('images/background/directivos.png')
-    context = {'title': 'Directivos', 'header_img': header_img}
+    context = {'title': _('Directivos'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/directivos.html', context)
 
 
 def comites(request):
     header_img = staticfiles_storage.url('images/background/comites.png')
-    context = {'title': 'Comités', 'header_img': header_img}
+    context = {'title': _('Comités'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/comites.html', context)
 
 
 def auditor_externo(request):
     header_img = staticfiles_storage.url('images/background/auditor-externo.png')
-    context = {'title': 'Auditor Externo', 'header_img': header_img}
+    context = {'title': _('Auditor Externo'), 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/auditor-externo.html', context)
 
 
 def prospectos_suplementos(request):
     header_img = staticfiles_storage.url('images/background/prospectos_suplementos.png')
-    context = {'title': 'Prospectos y Suplementos', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Prospectos y Suplementos'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/prospectos-suplementos.html', context)
 
 
 def asambleas(request):
     header_img = staticfiles_storage.url('images/background/asamblea.png')
-    context = {'title': 'Asambleas', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Asambleas'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/asambleas.html', context)
 
 
 def codigo_etica(request):
     header_img = staticfiles_storage.url('images/background/codigo-etica.png')
-    context = {'title': 'Código de Ética', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('Código de Ética'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/codigo-etica.html', context)
 
 
 def esg(request):
     header_img = staticfiles_storage.url('images/background/esg.png')
-    context = {'title': 'ESG', 'urlbase': urlbase, 'header_img': header_img}
+    context = {'title': _('ESG'), 'urlbase': urlbase, 'header_img': header_img}
     return render(request, f'frontend/{request.LANGUAGE_CODE}/gobierno/ESG.html', context)
 
 
